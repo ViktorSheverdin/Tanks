f7071fcd315774c9667e335a9c4b254e413c9936
Viktor Sheverdin <viktor.sheverdin@houle.ca>
2020-03-03 10:22:47 -0800

    Added functions to redraw and get_players_on_server

--- client2.py
+++ client2.py
@@ -7,45 +7,32 @@ width = 500
 height = 500
 window = pygame.display.set_mode((width,height))
 
-def redraw_window(window, current_player):
+def redraw_window(window, players_on_server):
     window.fill((255,255,255))
-    #pygame.draw.rect(window,obj_player.color, (obj_player.x,obj_player.y,obj_player.width,obj_player.height))
-    current_player.draw(window)
+    for each_player_key, each_player_value in players_on_server.items():
+        each_player_value.draw(window)
     pygame.display.update()
+
+def get_players_on_server(network_module,obj_player):
+    players_on_server = network_module.send({obj_player.playerID: obj_player})
+    print("#######################################")
+    print("players_on_server: ",players_on_server)
+    print("#######################################")
+    return players_on_server
 
 def main():
     network_module = Network()
     clock = pygame.time.Clock()
     obj_player = network_module.getPlayerInfo()
-    print("init obj_player: ", obj_player)
     
     isRunning = True
     while isRunning:
-        #print("Getting player info")
-        #print("New obj_play's ID is: \n", obj_player)
         clock.tick(60)
-
         obj_player.move()
-        #print("ojb_player send: ",network_module.send(obj_player))
-        players_on_server = network_module.send({obj_player.playerID: obj_player})
-        print("#######################################")
-        print("players_on_server: ",players_on_server)
-        print("#######################################")
-        
-        window.fill((255,255,255))
-        #for each_player_key, each_player_value in players_on_server.items():
-        for each_player_key, each_player_value in players_on_server.items():
-            print("each_player_value: ",each_player_value)
-            print()
-            #redraw_window(window, each_player)
-            #pygame.draw.rect(window,each_player.color, (each_player.x,each_player.y,each_player.width,each_player.height))
-            #redraw_window(window, each_player_value)
-            #redraw_window(window, obj_player)
-            pygame.draw.rect(window,each_player_value.color, (each_player_value.x,each_player_value.y,each_player_value.width,each_player_value.height))
-        pygame.display.update()
 
-            
-            
+        players_on_server = get_players_on_server(network_module,obj_player)
+        redraw_window(window,players_on_server)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 isRunning = False
--- server2.py
+++ server2.py
@@ -11,7 +11,6 @@ port = 5555
 gameID = 0
 new_playerID = 0
 games = {}
-#players_on_server = []
 players_on_server = {}
 
 # Creates a socket and init the server
@@ -31,39 +30,22 @@ def threaded_client(conn, playerID):
     new_player = Player(playerID, 150, 150, (255,0,200), 100, 100)
 
     print("Sending PlayerID")
-    #players_on_server.append(new_player)
     players_on_server.update({new_player.playerID: new_player})
     conn.send(pickle.dumps(new_player))
-    #conn.send(pickle.dumps({new_player.playerID: new_player}))
-    print("PlayerID was sent")
     
     reply = ""
     while True:
         try:
             data = pickle.loads(conn.recv(2048))
-            print("Received data when connected :\n", data)
             for key, value in data.items():
-                print("Key: ",key, " Value: ", value, " PlayerID: ", playerID, " players_on_server[value.playerID]", players_on_server[value.playerID])
                 if players_on_server[value.playerID].playerID == playerID:
-                #if players_on_server[key].playerID == playerID:
-                    print("Received playerID: ",value)
-                    #for players in players_on_server:
-                    #players_on_server.update(players_on_server[playerID] = value)
-                    print("players_on_server[value.playerID]",players_on_server[value.playerID])
                     players_on_server[value.playerID] = value
-                    print("New players_on_server: ", players_on_server)
-                    # if (value.playerID == playerID):                
-                    #     players_on_server.update(playerID = data)
-            print("players_on_server[playerID]",players_on_server[playerID])
             if not data:
                 print("Disconnected from the server")
-                #players_on_server.pop(int(data.playerID))
                 break
             else:
                 print("All players on the server: \n", players_on_server)
-            
                 conn.sendall(pickle.dumps(players_on_server))
-            #conn.send(pickle.dumps(players_on_server))
         except:
             print("Disconnected from the server")
             try:
